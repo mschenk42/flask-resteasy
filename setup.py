cc8a4f915ad651f1edc0a41abda5fb43737c5699
mschenk <mschenk@theschenks.name>
2014-12-21 12:35:00 -0600

    Created initial setup.py file
    This allow for installing as a package locally

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='flask-resteasy',
+    version='0.1',
+    packages=['flask_resteasy'],
+    url='',
+    license='BSD',
+    author='Michael Schenk',
+    author_email='',
+    description='',
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask', 'Flask-SQLAlchemy', 'inflection']
+)
