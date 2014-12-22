edbdd076eb7fff4127c06c29ccdf89f5c1aba1cd
mschenk <mschenk@theschenks.name>
2014-12-22 17:26:59 -0600

    Updated version info in setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from distutils.core import setup
 
 setup(
     name='flask-resteasy',
-    version='0.1',
+    version='0.0.1',
     packages=['flask_resteasy'],
-    url='',
-    license='BSD',
     author='Michael Schenk',
     author_email='',
     description='',
+    url='',
     zip_safe=False,
     include_package_data=True,
+    license='BSD',
     platforms='any',
-    install_requires=['Flask', 'Flask-SQLAlchemy', 'inflection']
+    install_requires=['Flask', 'Flask-SQLAlchemy', 'inflection'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Frameworks",
+        "License :: OSI Approved :: BSD License",
+    ],
 )
